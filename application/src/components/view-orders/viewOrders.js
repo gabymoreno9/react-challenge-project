06ca43cd7e33a5207b152b935bf6c2835e16141f
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-04-20 19:37:33 -0700

    solved the time bug on the view orders

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -3,6 +3,17 @@ import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
 import './viewOrders.css';
 
+// helper functions
+function formatWithZero (number) {
+    if (number < 10) {
+        return '0' + number
+    }
+    else {
+        return number
+    }
+}
+
+
 class ViewOrders extends Component {
     state = {
         orders: []
@@ -20,29 +31,33 @@ class ViewOrders extends Component {
             });
     }
 
+    getFormattedDate (date) {
+        const createdDate = new Date(date)
+        const minutes = createdDate.getMinutes()
+        const seconds = createdDate.getSeconds()
+        return `${createdDate.getHours()}:${formatWithZero(minutes)}:${formatWithZero(seconds)}`
+    }
+
     render() {
         return (
             <Template>
                 <div className="container-fluid">
-                    {this.state.orders.map(order => {
-                        const createdDate = new Date(order.createdAt);
-                        return (
-                            <div className="row view-order-container" key={order._id}>
-                                <div className="col-md-4 view-order-left-col p-3">
-                                    <h2>{order.order_item}</h2>
-                                    <p>Ordered by: {order.ordered_by || ''}</p>
-                                </div>
-                                <div className="col-md-4 d-flex view-order-middle-col">
-                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                                    <p>Quantity: {order.quantity}</p>
-                                 </div>
-                                 <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
-                                 </div>
+                    {this.state.orders.map(order =>
+                        <div className="row view-order-container" key={order._id}>
+                            <div className="col-md-4 view-order-left-col p-3">
+                                <h2>{order.order_item}</h2>
+                                <p>Ordered by: {order.ordered_by || ''}</p>
                             </div>
-                        );
-                    })}
+                            <div className="col-md-4 d-flex view-order-middle-col">
+                                <p>Order placed at {this.getFormattedDate(order.createdAt)}</p>
+                                <p>Quantity: {order.quantity}</p>
+                                </div>
+                                <div className="col-md-4 view-order-right-col">
+                                    <button className="btn btn-success">Edit</button>
+                                    <button className="btn btn-danger">Delete</button>
+                                </div>
+                        </div>
+                    )}
                 </div>
             </Template>
         );
